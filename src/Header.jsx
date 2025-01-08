50a26ed02bda25f6629724ca895457aed3a8df8b
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-01-07 21:01:11 -0700

    Started styling of header

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <div className="welcome">
+            {/* <div className="welcome">
             Welcome to
-            </div>
-            <div className = "title">
-            Bracelet Maker!
+            </div> */}
+            <div className = "title pattaya-regular">
+            Bracelet Maker
             </div>
             <div className = "subtitle">
                 Design your very own bracelets!
