141d0308215cfee1b10c2a5dbc5a4706f97655ca
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-01-09 19:38:22 -0700

    Drag and drop an image working

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Header />
-      <Navbar />
+      {/* <Navbar /> */}
       <Drag />
       {/* <Sample /> */}
     </div>
--- src/DragNDrop.jsx
+++ src/DragNDrop.jsx
@@ -11,12 +11,14 @@ const Drag = () => {
     function handleOnDrop(e){
         const widgetType = e.dataTransfer.getData("widgetType");
         console.log("Widget type", widgetType);
-        setWidgets([...widgets, widgetType]);
+        // setWidgets([...widgets, widgetType]);
+        setWidgets([widgetType]);
     }
     function handleOnDrop2(e){
         const widgetType = e.dataTransfer.getData("widgetType");
         console.log("Widget type", widgetType);
-        setWidgets2([...widgets2, widgetType]);
+        // setWidgets2([...widgets2, widgetType]);
+        setWidgets2([widgetType]);
     }
 
     function handleDragOver(e){
@@ -27,7 +29,17 @@ const Drag = () => {
     return (
         <div className="App">
             <div className="widgets">
-                <div className="widget"
+                <img className="bead" 
+                draggable
+                onDragStart={(e) => handleOnDrag(e, "hematite")}
+                src="/hematite.jpg"
+                alt="hematite bead" />
+                <img className="bead" 
+                draggable
+                onDragStart={(e) => handleOnDrag(e, "howlite")}
+                src="/howlite.jpg"
+                alt="howlite bead" />
+                {/* <div className="widget"
                 draggable
                 onDragStart={(e) => handleOnDrag(e, "Widget A")}
                 >Widget A
@@ -41,21 +53,28 @@ const Drag = () => {
                 draggable
                 onDragStart={(e) => handleOnDrag(e, "Widget C")}
                 >Widget C
+                </div> */}
+            </div>
+            <div>
+                <img className="bracelet" src="/hemp.jpg" alt="hemp bracelet" />
+                <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
+                    {widgets.map((widget, i) => (
+                        // <div className="droppedWidget" key={i}>
+                        <img src={`/${widget}.jpg`} />
+                        // </div>
+                    ))}
+                    {/* <div className="droppedWidget">
+                        {widgets}
+                        </div> */}
                 </div>
-            </div>
-            <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver}>
-                {widgets.map((widget, i) => (
-                    <div className="droppedWidget" key={i}>
-                        {widget}
-                    </div>
-                ))}
-            </div>
-            <div className="page" onDrop={handleOnDrop2} onDragOver={handleDragOver}>
-                {widgets2.map((widget, i) => (
-                    <div className="droppedWidget" key={i}>
-                        {widget}
-                    </div>
-                ))}
+                <div className="page" onDrop={handleOnDrop2} onDragOver={handleDragOver}>
+                    {widgets2.map((widget, i) => (
+                        <div className="droppedWidget" key={i}>
+                            {/* {widget} */}
+                            <img src={`/${widget}.jpg`} />
+                        </div>
+                    ))}
+                </div>
             </div>
 
         </div>
