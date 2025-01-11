58c0142700151deb0a73689d5fb93f7d8f6050fa
Anna Brooksby <johnhbrooksby@yahoo.com>
2025-01-11 00:31:20 -0700

    Sets drop areas over bracelet image, adds reset button

--- src/DragNDrop.jsx
+++ src/DragNDrop.jsx
@@ -3,7 +3,8 @@ import React, {useState} from "react";
 const Drag = () => {
     const [widgets, setWidgets] = useState([]);
     const [widgets2, setWidgets2] = useState([]);
-
+    const [widgets3, setWidgets3] = useState([]);
+    
     function handleOnDrag(e, widgetType){
         e.dataTransfer.setData("widgetType", widgetType);
     }
@@ -20,6 +21,12 @@ const Drag = () => {
         // setWidgets2([...widgets2, widgetType]);
         setWidgets2([widgetType]);
     }
+    function handleOnDrop3(e){
+        const widgetType = e.dataTransfer.getData("widgetType");
+        console.log("Widget type", widgetType);
+        // setWidgets2([...widgets2, widgetType]);
+        setWidgets3([widgetType]);
+    }
 
     function handleDragOver(e){
         e.preventDefault();
@@ -28,6 +35,39 @@ const Drag = () => {
 
     return (
         <div className="App">
+            <div className="placement">
+                <div className="braceletPlacement">
+                    <img className="bracelet" src="/hemp.jpg" alt="hemp bracelet" />
+                </div>
+                <div className="beadPlacement">
+                    <div className="page beadOne" onDrop={handleOnDrop} onDragOver={handleDragOver} onDoubleClick={() => setWidgets([])}>
+                        {widgets.map((widget, i) => (
+                            <div className="droppedWidget" key={i}>
+                                <img className="bead" src={`/${widget}.jpg`} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="page beadTwo" onDrop={handleOnDrop2} onDragOver={handleDragOver} onDoubleClick={() => setWidgets2([])}>
+                        {widgets2.map((widget, i) => (
+                            <div className="droppedWidget" key={i}>
+                                <img className="bead" src={`/${widget}.jpg`} />
+                            </div>
+                        ))}
+                    </div>
+                    <div className="page beadThree" onDrop={handleOnDrop3} onDragOver={handleDragOver} onDoubleClick={() => setWidgets3([])}>
+                        {widgets3.map((widget, i) => (
+                            <div className="droppedWidget" key={i}>
+                                <img className="bead" src={`/${widget}.jpg`} />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <button onClick={() => {
+                    setWidgets([])
+                    setWidgets2([])
+                    setWidgets3([])
+                }}>Reset</button>
+            </div>
             <div className="widgets">
                 <img className="bead" 
                 draggable
@@ -39,41 +79,6 @@ const Drag = () => {
                 onDragStart={(e) => handleOnDrag(e, "howlite")}
                 src="/howlite.jpg"
                 alt="howlite bead" />
-                {/* <div className="widget"
-                draggable
-                onDragStart={(e) => handleOnDrag(e, "Widget A")}
-                >Widget A
-                </div>
-                <div className="widget"
-                draggable
-                onDragStart={(e) => handleOnDrag(e, "Widget B")}
-                >Widget B
-                </div>
-                <div className="widget"
-                draggable
-                onDragStart={(e) => handleOnDrag(e, "Widget C")}
-                >Widget C
-                </div> */}
-            </div>
-            <div>
-                <img className="bracelet" src="/hemp.jpg" alt="hemp bracelet" />
-                <div className="page" onDrop={handleOnDrop} onDragOver={handleDragOver} onDoubleClick={() => setWidgets([])}>
-                    {widgets.map((widget, i) => (
-                        <div className="droppedWidget" key={i}>
-                            <img src={`/${widget}.jpg`} />
-                        </div>
-                    ))}
-                    {/* <div className="droppedWidget">
-                        {widgets}
-                        </div> */}
-                </div>
-                <div className="page" onDrop={handleOnDrop2} onDragOver={handleDragOver} onDoubleClick={() => setWidgets2([])}>
-                    {widgets2.map((widget, i) => (
-                        <div className="droppedWidget" key={i}>
-                            <img src={`/${widget}.jpg`} />
-                        </div>
-                    ))}
-                </div>
             </div>
 
         </div>
